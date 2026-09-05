5be91fb4bde6d2f5e2a2092003154cac93f41abe
jsdev <song.johan.2007@gmail.com>
2026-09-05 16:27:21 -0400

    fixed bug

--- src/scenes/modlist.lua
+++ src/scenes/modlist.lua
@@ -143,7 +143,7 @@ local function refreshVisibleMods()
             and
             -- only show leaf mods
             (not scene.onlyShowLeafMods
-                or not next(scene.modDependents[mod.info.Name]))
+                or not next(scene.modDependents[mod.info.Name] or {}))
             and
             -- search terms
             (scene.search == ""
